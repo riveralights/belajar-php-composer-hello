04a96fb9c49fb3f15a8d935752028e0b72c5a9fa
riveralights <riveralights@gmail.com>
2022-02-07 16:04:55 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FuadMuhammadNur\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+}
